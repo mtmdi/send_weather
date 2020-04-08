daf25b5f5850d710760c0049dbb682a05488fd0d
Ahmad Motamedi <ahmad.motamedi@gmail.com>
2020-04-08 05:50:57 +0430

    'define send ams function'

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import requests
+from ippanel import Client
 
-def send_sms(text, phonenumber):
-    
 
-response = requests.get('https://www.metaweather.com/api/location/2251945/')
-weather_state = response.json()['consolidated_weather'][0]['weather_state_name']
-humidity = response.json()['consolidated_weather'][0]['humidity']
-the_temp = response.json()['consolidated_weather'][0]['the_temp']
+def send_sms(text,phonenumber):
+    # you api key that generated from panel
+    api_key = ""
+    # create client instance
+    sms = Client(api_key)
+    phonenumber = ['98'+str(phonenumber)[1:]]
+    bulk_id = sms.send(
+    "+98500010706050404",# originator
+    phonenumber,    # recipients
+    text # message
+)
 
-print ('temperature in Tehran now is %i' %the_temp)
-print ('and humidity is %i' %humidity)
-print ('and the current weather is %s' %weather_state)
+
+responds = requests.get('https://www.metaweather.com/api/location/2251945/')
+time = responds.json()['time']
+weather_state = responds.json()['consolidated_weather'][0]['weather_state_name']
+humidity = responds.json()['consolidated_weather'][0]['humidity']
+the_temp = responds.json()['consolidated_weather'][0]['the_temp']
+message = """In Tehran:
+time is %s 
+humidity is %i percent
+the tempreture is %i C 
+and the weather status is %s.""" % (time[11:16], humidity, the_temp, weather_state)
+
+send_sms(message,'09120000000')
 
